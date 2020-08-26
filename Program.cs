d696dac1b2cda0b25007475e0e3ca78c3b54b75d
mddipiet <68350023+mddipiet@users.noreply.github.com>
2020-08-26 13:35:14 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab_7_4_DatingWebsite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatingProfile joey = new DatingProfile("Joey", "Tribiani", 30, "Male");
+            joey.WriteBio("Tall, Dark, and Handsome!");
+
+            DatingProfile 
+        }
+    }
+}
